4fef82a3cf5829ed6f209b49420a6bf2e6d8c489
BaZZcheesy <107484617+BaZZcheesy@users.noreply.github.com>
2024-05-06 10:55:18 +0200

    Added controllers etc...

---
+++ motoforum-api/src/main/java/ch/wiss/motoforumapi/controller/PrivateController.java
@@ -0,0 +1,14 @@
+package ch.wiss.motoforumapi.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PrivateController {
+
+    @GetMapping("private")
+    public ResponseEntity<String> getPrivatePart() {
+        return ResponseEntity.ok("Dies ist der Private Teil ");
+    }
+}
---
+++ motoforum-api/src/main/java/ch/wiss/motoforumapi/setup/DataBaseLoader.java
@@ -0,0 +1,44 @@
+package ch.wiss.motoforumapi.setup;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import ch.wiss.motoforumapi.repository.RoleRepository;
+import ch.wiss.motoforumapi.repository.UserRepository;
+import ch.wiss.motoforumapi.security.ERole;
+import ch.wiss.motoforumapi.security.Role;
+import ch.wiss.motoforumapi.security.User;
+
+@Component
+public class DataBaseLoader implements CommandLineRunner {
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+
+    @Autowired
+    public DataBaseLoader(PasswordEncoder passwordEncoder, RoleRepository roleRepository,
+            UserRepository userRepository) {
+        this.passwordEncoder = passwordEncoder;
+        this.roleRepository = roleRepository;
+        this.userRepository = userRepository;
+    }
+
+    // Hier werden die Testdaten erstellt und in die Datenbank gespeichert.
+    @Override
+    public void run(String... strings) throws Exception {
+        if (roleRepository.count() > 0) {
+            return;
+        }
+        this.roleRepository.save(new Role(ERole.ROLE_USER));
+        this.roleRepository.save(new Role(ERole.ROLE_ADMIN));
+        User user = new User("janedoe", "jane.doe@email.com", passwordEncoder.encode("p@ssw0rd"));
+        user.setRoles(new HashSet<>(Arrays.asList(
+                roleRepository.findByRole(ERole.ROLE_USER).get())));
+        this.userRepository.save(user);
+    }
+}
